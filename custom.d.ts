f875293fb712b90d6aadc6f1728b5226a03e2160
Rauf <raufislam2003@gmail.com>
2019-09-01 20:13:32 -0400

    custom events in the works

--- custom.d.ts
+++ custom.d.ts
@@ -3,5 +3,6 @@ import { IInfraction } from "./src/models/Infraction";
 declare module 'discord.js' {
   export interface Client {
     on(event: "addUserInfraction", listener: (user: GuildMember, infraction: IInfraction) => void): this;
+	on(event: "addUserBlacklist", listener: (user: GuildMember, reason?: string) => void): this;
   }
 }
