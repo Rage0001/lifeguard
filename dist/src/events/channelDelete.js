f3081277f59a378e2abeee61b0bcab571bcd857e
Rauf <raufislam2003@gmail.com>
2019-09-12 21:55:32 -0400

    channel delete

---
+++ dist/src/events/channelDelete.js
@@ -0,0 +1,31 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const discord_js_1 = require("discord.js");
+const Guild_1 = require("../models/Guild");
+const Event_1 = require("./Event");
+exports.event = new Event_1.Event("channelDelete", async (bot, channel) => {
+    const lang = bot.langs["en-US"].events.channelCreate;
+    try {
+        const guild = await Guild_1.findGuild(channel.guild.id);
+        const embed = new discord_js_1.RichEmbed({
+            description: bot.format(lang.log, {
+                id: channel.id,
+                name: `#${channel.name}`
+            }),
+            title: lang.title
+        });
+        embed.setTimestamp();
+        if (guild) {
+            const modlog = channel.guild.channels.get(guild.modLog);
+            if (modlog) {
+                modlog.send(embed);
+            }
+        }
+    }
+    catch (err) {
+        return {
+            location: "ChannelCreate Event",
+            message: err
+        };
+    }
+});
--- shard.ts
+++ shard.ts
@@ -4,4 +4,4 @@ const Manager = new ShardingManager("./dist/index.js");
 Manager.spawn(3, 20000);
 Manager.on("launch", (shard) => {
   console.log(`Started ${shard.id}`);
-})
+});
