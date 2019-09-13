6f76eb7ab6f962c509ad3f588b7ae7e36c287ca7
Rauf <raufislam2003@gmail.com>
2019-09-12 21:48:57 -0400

    more sharding fix stuff i hate this sharding bs

--- dist/shard.js
+++ dist/shard.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const discord_js_1 = require("discord.js");
 const Manager = new discord_js_1.ShardingManager("./dist/index.js");
-Manager.spawn(5);
+Manager.spawn(3, 20000);
 Manager.on("launch", (shard) => {
     console.log(`Started ${shard.id}`);
 });
--- shard.ts
+++ shard.ts
@@ -1,7 +1,7 @@
 import { ShardingManager } from "discord.js";
 const Manager = new ShardingManager("./dist/index.js");
 
-Manager.spawn(5);
+Manager.spawn(3, 20000);
 Manager.on("launch", (shard) => {
   console.log(`Started ${shard.id}`);
 })
