62f8d3a2d5aa126cabfee7181943eab43e965a12
Rauf <raufislam2003@gmail.com>
2019-09-25 16:02:47 -0400

    guildBanAdd Event

---
+++ src/events/guildBanAdd.ts
@@ -0,0 +1,30 @@
+import { Guild, RichEmbed, TextChannel, User } from "discord.js";
+import { findGuild } from "../models/Guild";
+import { Event } from "./Event";
+
+export const event = new Event("guildBanAdd", async (bot, guild: Guild, user: User) => {
+  const lang = bot.langs["en-US"].events.guildBanAdd;
+  try {
+    const dbGuild = await findGuild(guild.id);
+    const embed = new RichEmbed({
+      description:
+        bot.format(lang.log, {
+          id: user.id,
+          name: `<@${user.id}>`
+        }),
+      title: lang.title
+    });
+    embed.setTimestamp();
+    if (dbGuild) {
+      const modlog = guild.channels.get(dbGuild.modLog);
+      if (modlog) {
+        (modlog as TextChannel).send(embed);
+      }
+    }
+  } catch (err) {
+    return {
+      location: "GuildBanAdd Event",
+      message: err
+    };
+  }
+});
