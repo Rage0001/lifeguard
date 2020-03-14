d701256ebdf922600e1e2a96774af6c4a32339e8
ItsRauf <raufislam2003@gmail.com>
2020-03-14 14:47:52 -0400

    feat(events/ready): show git commit when in development

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,12 +1,26 @@
 import { Event } from '@events/Event';
+import { promisify } from 'util';
+import { exec } from 'child_process';
 
-export const event = new Event('ready', lifeguard => {
+export const event = new Event('ready', async lifeguard => {
   console.log('Connected to Discord');
-  if (lifeguard.user) {
-    lifeguard.user.setPresence({
+  if (process.env.NODE_ENV !== 'DEV') {
+    lifeguard.user?.setPresence({
       activity: {
         name: `${lifeguard.users.cache.size} people in the pool • lg.help`,
         type: 'WATCHING',
+      },
+      status: 'online',
+    });
+  } else {
+    const run = promisify(exec);
+    const { stdout: gitCommitString } = await run(
+      "git log -n 1 --pretty='format:%C(auto)%h | %s'"
+    );
+    lifeguard.user?.setPresence({
+      activity: {
+        name: gitCommitString,
+        type: 'PLAYING',
       },
       status: 'online',
     });
