c50e6f7238562710c5660fc7902a5e128375c637
ItsRauf <raufislam2003@gmail.com>
2021-03-20 02:55:30 -0400

    fix(events/ready): use new logger

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -3,6 +3,6 @@ import { Event, EventFunc } from "@events/Event.ts";
 export default class extends Event<"ready"> {
   name = "ready" as const;
   func: EventFunc<"ready"> = (ctx) => {
-    ctx.log("Lifeguard is ready!");
+    ctx.logger.info("Lifeguard is ready!");
   };
 }
