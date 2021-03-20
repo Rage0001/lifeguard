0a990920d305a62c93f89ff4b1edfe14638b7e74
ItsRauf <raufislam2003@gmail.com>
2021-03-20 01:42:18 -0400

    fix(env): actual have env vars working

--- main.ts
+++ main.ts
@@ -1,11 +1,13 @@
-import { Lifeguard } from "/src/bot.ts";
+import { Lifeguard } from "./src/bot.ts";
 import { config } from ".env";
 
 if (!import.meta.main) {
   throw new Error("main.ts must be the program entry point!");
 }
 
-config({
-  path: Deno.env.get("LG_ENV")?.startsWith("dev") ? ".env.devel" : ".env",
+const env = config({
+  path: Deno.env.get("LG_ENV")?.startsWith("dev") ? "./.env.devel" : "./.env",
+  safe: true,
 });
-new Lifeguard().connect(Deno.env.get("TOKEN"));
+
+new Lifeguard().connect(env.TOKEN);
