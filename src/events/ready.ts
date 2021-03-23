a781bfb281eaded18beb29f0431114e22931015a
ItsRauf <raufislam2003@gmail.com>
2021-03-23 03:42:20 -0400

    feat(events/ready): Insert Lifeguard User into the Database

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -2,7 +2,12 @@ import { Event, EventFunc } from "@events/Event.ts";
 
 export default class extends Event<"ready"> {
   name = "ready" as const;
-  func: EventFunc<"ready"> = (ctx) => {
+  func: EventFunc<"ready"> = async (ctx) => {
     ctx.logger.info("Lifeguard is ready!");
+    // Insert Lifeguard User into the Database
+    await ctx.db.Users.insert({
+      id: ctx.lifeguard.user?.id!,
+      blocked: false,
+    });
   };
 }
